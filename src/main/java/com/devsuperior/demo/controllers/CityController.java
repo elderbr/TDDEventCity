bc272d54a08096374df7a8bb33d52750a3560532
ElderBR <elderbarbosaromao@gmail.com>
2024-01-13 18:56:01 -0300

    Criado o método para deletar a City

--- src/main/java/com/devsuperior/demo/controllers/CityController.java
+++ src/main/java/com/devsuperior/demo/controllers/CityController.java
@@ -2,7 +2,9 @@ package com.devsuperior.demo.controllers;
 
 import com.devsuperior.demo.dto.CityDTO;
 import com.devsuperior.demo.services.CityService;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -28,5 +30,17 @@ public class CityController {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
                 .buildAndExpand(dto.getId()).toUri();
         return ResponseEntity.created(uri).body(dto);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        try {
+            service.delete(id);
+            return ResponseEntity.noContent().build();
+        }catch (EntityNotFoundException e){
+            return ResponseEntity.notFound().build();
+        } catch (DataIntegrityViolationException e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 }
--- src/main/java/com/devsuperior/demo/services/CityService.java
+++ src/main/java/com/devsuperior/demo/services/CityService.java
@@ -3,9 +3,12 @@ package com.devsuperior.demo.services;
 import com.devsuperior.demo.dto.CityDTO;
 import com.devsuperior.demo.entities.City;
 import com.devsuperior.demo.repositories.CityRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -18,16 +21,24 @@ public class CityService {
     private CityRepository repository;
 
     @Transactional(readOnly = true)
-    public List<CityDTO> findAllByOrderByName(){
+    public List<CityDTO> findAllByOrderByName() {
         List<City> city = repository.findAll(Sort.by("name"));
-        return city.stream().map(x->new CityDTO(x)).collect(Collectors.toList());
+        return city.stream().map(x -> new CityDTO(x)).collect(Collectors.toList());
     }
 
     @Transactional
-    public CityDTO insert(CityDTO dto){
+    public CityDTO insert(CityDTO dto) {
         City city = new City();
         city.setName(dto.getName());
         city = repository.save(city);
         return new CityDTO(city);
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public void delete(long id) throws EntityNotFoundException, DataIntegrityViolationException {
+        if (!repository.existsById(id)) {
+            throw new EntityNotFoundException("Recurso não encontrado!");
+        }
+        repository.deleteById(id);
     }
 }
