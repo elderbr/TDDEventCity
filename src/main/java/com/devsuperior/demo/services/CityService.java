a519cae044018d9458a8a16d15230aa6bac20c50
ElderBR <elderbarbosaromao@gmail.com>
2024-01-13 18:17:20 -0300

    Criado o método para inserir nova City

--- src/main/java/com/devsuperior/demo/services/CityService.java
+++ src/main/java/com/devsuperior/demo/services/CityService.java
@@ -6,6 +6,7 @@ import com.devsuperior.demo.repositories.CityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,8 +17,17 @@ public class CityService {
     @Autowired
     private CityRepository repository;
 
+    @Transactional(readOnly = true)
     public List<CityDTO> findAllByOrderByName(){
         List<City> city = repository.findAll(Sort.by("name"));
         return city.stream().map(x->new CityDTO(x)).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public CityDTO insert(CityDTO dto){
+        City city = new City();
+        city.setName(dto.getName());
+        city = repository.save(city);
+        return new CityDTO(city);
     }
 }
