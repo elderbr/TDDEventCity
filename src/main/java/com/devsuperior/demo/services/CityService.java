e9a35178544881aea600797ef521cafdea0667c8
ElderBR <elderbarbosaromao@gmail.com>
2024-01-13 18:04:12 -0300

    Criando o service da City e fazendo a busca por todas City organizando pelo o nome

--- src/main/java/com/devsuperior/demo/services/CityService.java
+++ src/main/java/com/devsuperior/demo/services/CityService.java
@@ -1,2 +1,23 @@
-package com.devsuperior.demo.services;public class CityService {
+package com.devsuperior.demo.services;
+
+import com.devsuperior.demo.dto.CityDTO;
+import com.devsuperior.demo.entities.City;
+import com.devsuperior.demo.repositories.CityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CityService {
+
+    @Autowired
+    private CityRepository repository;
+
+    public List<CityDTO> findAll(){
+        List<City> city = repository.findAll(Sort.by("name"));
+        return city.stream().map(x->new CityDTO(x)).collect(Collectors.toList());
+    }
 }
